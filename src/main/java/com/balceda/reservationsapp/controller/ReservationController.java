7b9c92128d1e23b1bdd91496d73c1e9b64ed2836
Juan Balceda <juanbalceda@gmail.com>
2018-12-29 22:43:59 +0100

    Reservation swagger-documentation added

--- src/main/java/com/balceda/reservationsapp/controller/ReservationController.java
+++ src/main/java/com/balceda/reservationsapp/controller/ReservationController.java
@@ -18,8 +18,14 @@ import com.balceda.reservationsapp.controller.vo.ReservationVO;
 import com.balceda.reservationsapp.model.Reservation;
 import com.balceda.reservationsapp.service.ReservationService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/api/reservation")
+@Api(tags = "reservation")
 public class ReservationController {
 
 	private final ReservationService reservationService;
@@ -29,6 +35,9 @@ public class ReservationController {
 	}
 
 	@PostMapping
+	@ApiOperation(value = "Create a reservation", notes = "This service creates a new reservation")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Reservation created successfully"),
+			@ApiResponse(code = 400, message = "Invalid request") })
 	public ResponseEntity<Reservation> createReservation(@RequestBody ReservationVO reservationVO) {
 		Reservation reservation = new Reservation();
 		reservation.setDateIn(reservationVO.getDateIn());
@@ -42,6 +51,9 @@ public class ReservationController {
 	}
 
 	@PutMapping("/{idCode}")
+	@ApiOperation(value = "Update a reservation", notes = "This service updates an existing reservation")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Reservation updated successfully"),
+			@ApiResponse(code = 404, message = "Client not found") })
 	public ResponseEntity<Reservation> updateReservation(@PathVariable("idCode") String idCode,
 			ReservationVO reservationVO) {
 
@@ -61,6 +73,9 @@ public class ReservationController {
 	}
 
 	@DeleteMapping("/{idCode}")
+	@ApiOperation(value = "Delete a reservation", notes = "This service deletes an existing reservation")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Reservation deleted successfully"),
+			@ApiResponse(code = 404, message = "Client not found") })
 	public void removeReservation(@PathVariable("idCode") String idCode) {
 		Reservation reservation = this.reservationService.findByIdCode(idCode);
 		if (reservation != null) {
@@ -69,6 +84,9 @@ public class ReservationController {
 	}
 
 	@GetMapping
+	@ApiOperation(value = "List all reservations", notes = "This service lists all existing reservations")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Reservations listed successfully"),
+			@ApiResponse(code = 404, message = "Reservations not found") })
 	public ResponseEntity<List<Reservation>> findAllReservations() {
 		List<Reservation> reservations = this.reservationService.findAll();
 		if (reservations != null) {
@@ -78,6 +96,9 @@ public class ReservationController {
 	}
 
 	@GetMapping("/{idCode}")
+	@ApiOperation(value = "Find a reservation by id", notes = "This service gets an existing reservation by a given id_code")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Reservation found"),
+			@ApiResponse(code = 404, message = "Reservation not found") })
 	public ResponseEntity<Reservation> findByIdNumberClient(@PathVariable("idCode") String idCode) {
 		Reservation reservation = this.reservationService.findByIdCode(idCode);
 		if (reservation != null) {
@@ -87,6 +108,9 @@ public class ReservationController {
 	}
 
 	@GetMapping("/in/{dateIn}/out/{dateOut}")
+	@ApiOperation(value = "List all reservations between dates", notes = "This service lists all existing reservations between dates")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Reservations listed successfully"),
+			@ApiResponse(code = 404, message = "Reservations not found") })
 	public ResponseEntity<List<Reservation>> findByLastNameClient(@PathVariable("dateIn") Date dateIn,
 			@PathVariable("dateOut") Date dateOut) {
 		List<Reservation> reservations = this.reservationService.find(dateIn, dateOut);
